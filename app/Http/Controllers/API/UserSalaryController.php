e41c076f174014500d153d5dedadd9b5e9c0120a
markangeloterante123 <markangeloterante@gmail.com>
2021-11-24 17:02:11 +0800

    Update

--- app/Http/Controllers/API/UserSalaryController.php
+++ app/Http/Controllers/API/UserSalaryController.php
@@ -4,8 +4,47 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Validator;
+use App\Models\User;
+use App\Models\UserInformation;
+use App\Models\UserSalary;
+use App\Http\Resources\UserSalaryResources;
+
 
 class UserSalaryController extends Controller
 {
     //
+    public function index()
+    {
+        $UserSalary = UserSalary::latest()->get();
+        return response()->json(['All User Salary Records', UserSalaryResources::collection($UserSalary)]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'user_id'=> 'required',
+            'payslip_link' => 'required',
+            'basic_salary' => 'required'
+        ]);
+    
+        if($validator->fails()){
+            return response()->json($validator->errors());       
+        }
+
+        $UserSalary = UserSalary::create($request->all());
+        
+        return response()->json(['Successfully Create Employee Salary Records.', new UserSalaryResources($UserSalary)]);
+    }
+    
+    public function show($id)
+    {
+        $UserSalary = UserSalary::where('user_id', $id)->get();
+
+        if (is_null($UserSalary)) {
+            return response()->json('Data not found', 404); 
+        }
+        return response()->json([UserSalaryResources::collection($UserSalary)]);
+    }
+
 }
--- app/Models/UserSalary.php
+++ app/Models/UserSalary.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class user_salary extends Model
+class UserSalary extends Model
 {
     protected $guarded = [];
     use HasFactory;
